52ec4dd24210cc34e8100507f6f0d9eb627b6695
Christoph Borgolte <christoph.borgolte@gmail.com>
2016-12-29 11:17:27 +0100

    Cleanup / more tests.

--- tests/test_tracker.py
+++ tests/test_tracker.py
@@ -10,7 +10,7 @@ def test_parse_task():
     line = (
         " 09:00 - 11.33 \tMyCustomer:  Ticket-1234 Debugging Workflow Engine")
     trk = tracker.Tracker()
-    trk.day = datetime.date(2016, 12, 31)
+    trk.set_day(datetime.date(2016, 12, 31))
     entry = trk.parse_task(line)
     assert entry['start'] == datetime.datetime(2016, 12, 31, 9)
     assert entry['stop'] == datetime.datetime(2016, 12, 31, 11, 33)
@@ -20,6 +20,24 @@ def test_parse_task():
 
 def test_parse_date():
     line = "Thursday, 26.10."  # TODO: thursday need's to be validated as wrong
+    trk = tracker.Tracker()
+    entry = trk.parse_new_date(line)
+    assert trk.day == datetime.date(2016, 10, 26)
+    assert entry['type'] == 'new_date'
+
+    line = "Mo., 26.10."
+    trk = tracker.Tracker()
+    entry = trk.parse_new_date(line)
+    assert trk.day == datetime.date(2016, 10, 26)
+    assert entry['type'] == 'new_date'
+
+    line = "Mo., 26.10.2015"
+    trk = tracker.Tracker()
+    entry = trk.parse_new_date(line)
+    assert trk.day == datetime.date(2015, 10, 26)
+    assert entry['type'] == 'new_date'
+
+    line = "di 26.10."
     trk = tracker.Tracker()
     entry = trk.parse_new_date(line)
     assert trk.day == datetime.date(2016, 10, 26)
--- yatt/yatt.py
+++ yatt/yatt.py
@@ -1,7 +1,1 @@
 # -*- coding: utf-8 -*-
-
-class FirstClass(object):
-    def func(self, a):
-        print(a)
-
-
